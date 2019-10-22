bb1959346ed68a8d551bca30d4e0eeca8d477912
rajdip@123 <rajdipwebpatriot>
2019-10-22 11:34:48 +0530

    add favicon

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -3,11 +3,13 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Admin Panel </title>
+
+  <link rel="icon" href="<?php echo base_url().'assets/webTitleIcon.png';?>" type="image/gif" sizes="16x16">
 
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-  <title>Admin Panel</title>
 
   <!-- Bootstrap 3.3.6 -->
   <link rel="stylesheet" href="<?php echo base_url().'assets/bootstrap/css/bootstrap.min.css'?>"/>
