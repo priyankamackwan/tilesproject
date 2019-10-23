a636b8e631770252b3a9dd70ff65fbf3c8918785
rajdip@123 <rajdipwebpatriot>
2019-10-23 13:31:53 +0530

    display lpo no in pdf file

--- application/controllers/Apis.php
+++ application/controllers/Apis.php
@@ -950,8 +950,15 @@ $html = '<html>
 <table style="width:100%;"><tr><td style="width:60%;">Customer : '.$userData[0]['company_name'].'</td><td style="width:40%; text-align:right;">Tel : '.$userData[0]['phone_no'].'</td></tr></table>
 <br><br/>
 <table style="width:100%;"><tr><td style="width:60%;">LPO No. : '.$lpo.'</td><td style="width:40%; text-align:right;">Invoice No. : '.$invoice.'</td></tr></table>
-    <br><br/>
- <table style="width:100%;"><tr><td style="width:60%;">Cargo : '.$data['cargo'].'</td><td style="width:40%; text-align:right;">Cargo Number : '.$data['cargo_number'].'</td></tr></table>  
+    <br><br/>';
+
+if(trim($customer_lpo)!="") // if customer lpo field not null then display it'
+{
+    $html.='<table style="width:100%;"><tr><td style="width:60%;">Customer LPO No. : '.$customer_lpo.'</td><td style="width:40%; text-align:right;"></td></tr></table>
+    <br><br/>';
+}
+
+$html.='<table style="width:100%;"><tr><td style="width:60%;">Cargo : '.$data['cargo'].'</td><td style="width:40%; text-align:right;">Cargo Number : '.$data['cargo_number'].'</td></tr></table>  
     <br><br/>
  <table style="width:100%;"><tr><td style="width:60%;">Location : '.$data['location'].'</td><td style="width:40%; text-align:right;">Mark : '.$data['mark'].'</td></tr></table>     
 <br><br/>
@@ -999,8 +1006,22 @@ $html1 = '<html>
 <table style="width:100%;"><tr><td style="width:40%;">LPO : '.$lpo.'</td><td style="width:60%; text-align:right;">Address : INDUSTRIAL AREA 2,<br>
     RAS AL KHOR, PO BOX: 103811 DUBAI-UAE</td> </tr>
     <tr><td style="width:100%; text-align:right;">Email : info@pnptiles.com</td></tr></table>
-<br><br/>
-<table style="width:100%;"><tr><td style="width:60%;">Customer VAT # : '.$userData[0]['vat_number'].'</td><td style="width:40%; text-align:right;">VAT ID # : 100580141800003</td> </tr></table>
+<br><br/>';
+
+if(trim($customer_lpo)!="") { // if customer lpo is exist then display it.
+$html1.='<table style="width:100%;"><tr><td style="width:40%;">LPO : '.$lpo.'</td><td style="width:60%; text-align:right;">Address : INDUSTRIAL AREA 2,</td></tr></table>
+<table style="width:100%;"><tr><td style="width:40%;">Customer LPO No. : '.$customer_lpo.'</td><td style="width:60%; text-align:right;">RAS AL KHOR, PO BOX: 103811 DUBAI-UAE</td> </tr>
+    <tr><td style="width:100%; text-align:right;">Email : info@pnptiles.com</td></tr></table>
+    <br><br/>'; } else {
+
+$html1.='<table style="width:100%;"><tr><td style="width:40%;">LPO : '.$lpo.'</td><td style="width:60%; text-align:right;">Address : INDUSTRIAL AREA 2,<br/>
+    RAS AL KHOR, PO BOX: 103811 DUBAI-UAE</td> </tr>
+    <tr><td style="width:100%; text-align:right;">Email : info@pnptiles.com</td></tr></table>
+    <br><br/>';
+}
+
+
+$html1.='<table style="width:100%;"><tr><td style="width:60%;">Customer VAT # : '.$userData[0]['vat_number'].'</td><td style="width:40%; text-align:right;">VAT ID # : 100580141800003</td> </tr></table>
 <br><br/>
 <table style="width:100%;" border="1"><tr><th style="text-align: center" width="5%">SR No.</th><th style="text-align: center" width="30%">DESCRIPTION</th><th style="text-align: center" width="10%">SIZE</th><th style="text-align: center" width="10%">DESIGN</th><th style="text-align: center" width="10%">UNIT</th><th style="text-align: center" width="13%">QUANTITY</th><th style="text-align: center" width="10%".>RATE</th><th style="text-align: center" width="12%">AMOUNT</th></tr>';
 $count = 0;
@@ -1045,8 +1066,15 @@ $html2 = '<html>
 <table style="width:100%;"><tr><td style="width:60%;">Invoice No. : '.$invoice.'</td><td style="width:40%; text-align:right;">Customer : '.$userData[0]['company_name'].'</td> </tr></table>
 <br><br/>
 <table style="width:100%;"><tr><td style="width:60%;">Tel. : '.$userData[0]['phone_no'].'</td><td style="width:40%; text-align:right;">LPO : '.$lpo.'</td> </tr></table>
-<br><br/>
-<table style="width:100%;"><tr><td style="width:60%;">Customer VAT # : '.$userData[0]['vat_number'].'</td><td style="width:40%; text-align:right;">VAT ID # : 100580141800003</td> </tr></table>
+<br><br/>';
+
+if(trim($customer_lpo)!="") // if customer lpo field not null then display it.
+{
+    $html2.='<table style="width:100%;"><tr><td style="width:60%;">Customer LPO No. : '.$customer_lpo.'</td><td style="width:40%; text-align:right;"></td></tr></table>
+    <br><br/>';
+}
+
+$html2.='<table style="width:100%;"><tr><td style="width:60%;">Customer VAT # : '.$userData[0]['vat_number'].'</td><td style="width:40%; text-align:right;">VAT ID # : 100580141800003</td> </tr></table>
 <br><br/>
 <table style="width:100%;" border="1"><tr><th style="text-align: center" width="5%">SR No.</th><th style="text-align: center" width="35%">DESCRIPTION</th><th style="text-align: center" width="10%">SIZE</th><th style="text-align: center" width="10%">DESIGN</th><th style="text-align: center" width="10%">UNIT</th><th style="text-align: center" width="10%">QUANTITY</th><th style="text-align: center" width="10%">RATE</th><th style="text-align: center" width="10%">AMOUNT</th></tr>';
 $count = 0;
