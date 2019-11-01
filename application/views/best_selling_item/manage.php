f54ca09084dcf45907d0f5ed12229a5535bee1d0
rajdip@123 <rajdipwebpatriot>
2019-11-01 16:02:20 +0530

    amount align roght

--- application/views/best_selling_item/manage.php
+++ application/views/best_selling_item/manage.php
@@ -321,12 +321,12 @@
 			},
       {
           "className": 'text-right',
-          "targets":   [],
+          "targets":   [6,7,8,9],
           "orderable": false
       },
       {
           "className": 'text-center',
-          "targets":   [0,1,,2,3,4,5,6,7,8,9],
+          "targets":   [0,1,,2,3,4,5],
           "orderable": false
       }],
 			"rowCallback": function( row, data, index ) {
