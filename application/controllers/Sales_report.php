2f89b36df2a7c590365158785f7968db78d01ac2
rajdip@123 <rajdipwebpatriot>
2020-01-08 16:04:22 +0530

    rate changes and amount from order product table

--- application/controllers/Sales_report.php
+++ application/controllers/Sales_report.php
@@ -359,7 +359,9 @@ echo $this->db->last_query();*/
                         $finalOrderData[$k]['description'] = $productData[0]['name'];
                         $finalOrderData[$k]['size'] = $productData[0]['size'];
                         $finalOrderData[$k]['design_no'] = $productData[0]['design_no'];
-                        
+                        //product price from order products table
+                        $finalOrderData[$k]['rate'] = $productOrder[$k]['price'];
+                        /*
                         if ($userData[0]['client_type'] == 1) {
                             $finalOrderData[$k]['rate'] = $productData[0]['cash_rate'];
                         }
@@ -375,7 +377,7 @@ echo $this->db->last_query();*/
                         if ($userData[0]['client_type'] == 4) {
                             $finalOrderData[$k]['rate'] = $productData[0]['flexible_rate'];
                         }
-                        
+                        */
                         if ($productData[0]['unit'] == 1) {
                             $finalOrderData[$k]['unit'] = 'CTN';
                         }
